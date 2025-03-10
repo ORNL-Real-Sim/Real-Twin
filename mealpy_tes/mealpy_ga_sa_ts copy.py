e2a73ed1045ac827848d8a61946dd65cc817dc88
xyluo25 <rochemay@163.com>
2025-03-10 16:00:13 -0400

    add MIT license

--- mealpy_tes/mealpy_ga_sa_ts copy.py
+++ mealpy_tes/mealpy_ga_sa_ts copy.py
@@ -16,7 +16,7 @@ from functools import partial
 from datetime import datetime
 import shutil
 
-from mealpy import FloatVar, SA, Tuner
+from mealpy import FloatVar, SA, GA, TS
 from util_cali_behavior import (get_travel_time_from_EdgeData_xml,
                                 update_flow_xml_from_solution,
                                 run_jtrrouter_to_create_rou_xml,
@@ -82,18 +82,59 @@ term_dict = {
     "max_epoch": 500,  # max iterations
     "max_fe": 10000,  # max function evaluations
     # "max_time": 3600,  # max time in seconds
-    "max_early_stop": 50,
+    "max_early_stop": 20,
 }
 
 # init_vals = create_starting_solutions(n_dims=6, pop_size=50, num=1)
-init_vals = np.array([2.5, 2.6, 4.5, 0.5, 1.0, 9.0] * 50).reshape(50, 6)
+init_vals = np.array([2.5, 2.6, 4.5, 0.5, 1.0, 9.0] * 2).reshape(2, 6)
 
-model = SA.GaussianSA(epoch=1000, pop_size=50, temp_init=100, scale=0.1)
-# model = SA.OriginalSA(epoch=1000, pop_size=2, temp_init=100, step_size=0.1,)
+# MODELS
+# Simulated Annealing
+# pop_size have no effect on the SA algorithm
+# model = SA.GaussianSA(epoch=1000, pop_size=50, temp_init=100, scale=0.1)
+model_sa = SA.OriginalSA(epoch=1000, pop_size=2, temp_init=100, step_size=0.1,)
 # model = SA.SwarmSA(epoch=1000, pop_size=50, max_sub_iter=5, t0=500, t1=1,
 #                    move_count=5, mutation_rate=0.1, mutation_step_size=0.1, mutation_step_size_damp=0.99)
-# g_best = model.solve(problem_dict, termination=term_dict, mode="single")
-g_best = model.solve(problem_dict, termination=term_dict, starting_solutions=init_vals)
 
+# Genetic Algorithm
+model_ga = GA.BaseGA(epoch=1000,
+                     pop_size=50,
+                     pc=0.95,  # crossover probability
+                     pm=0.025,  # mutation probability
+                     selection="tournament",  # "roulette", "tournament", "random"
+                     k_way=0.2,  # k-way for tournament selection
+                     crossover="uniform",  # one_point, multi-point, uniform, arithmetic
+                     mutation_multipoints=True,
+                     )  # mutation, flip, swap, scramble, inversion.
+# Elite Single GA: elite single-point mutation
+# models_gs = GA.EliteSingleGA(epoch=1000,
+#                              pop_size=50,
+#                              pc=0.95,  # crossover probability
+#                              pm=0.025,  # mutation probability
+#                              selection="tournament",  # "roulette", "tournament", "random"
+#                              crossover="uniform",  # one_point, multi-point, uniform, arithmetic
+#                              mutation="flip",  # flip, swap, scramble, inversion.
+#                              k_way=0.2,  # k-way for tournament selection
+#                              elite_best=0.1,  # percentage of the best in elite group, or int, the number of best elite
+#                              elite_worst=0.3,  # percentage of the worst in elite group, or int, the number of worst elite
+#                              )
+# EliteMultiGA: elite multipoints-mutation
+# MultiGA: multipoints-mutation
+# SingleGA: single-point-mutation
+
+# Tabu Search
+model_ts = TS.OriginalTS(epoch=1000,
+                         pop_size=2,  # This parameter has no effect on the TS algorithm
+                         tabu_size=10,  # maximum size of tabu list
+                         neighbour_size=10,  # size of the neighborhood for generating candidate solutions
+                         perturbation_scale=0.05  # scale of perturbation for generating candidate solutions
+                         )
+
+# g_best = model_sa.solve(problem_dict, termination=term_dict, mode="single")
+# g_best = model_sa.solve(problem_dict, termination=term_dict, starting_solutions=init_vals)
+
+# g_best = model_ga.solve(problem_dict, termination=term_dict, mode="single")
+
+g_best = model_ts.solve(problem_dict, termination=term_dict, mode="single")
 print(g_best.solution)
 print(g_best.target.fitness)
