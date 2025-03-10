e5669e248610ca013e43cd0e37ef264790da1234
xyluo25 <rochemay@163.com>
2025-03-10 16:08:23 -0400

    update copyright

--- realtwin/_realtwin.py
+++ realtwin/_realtwin.py
@@ -1,13 +1,12 @@
 ##############################################################################
-# Copyright (c) 2024, Oak Ridge National Laboratory                          #
+# Copyright (c) 2024-, Oak Ridge National Laboratory                          #
 # All rights reserved.                                                       #
 #                                                                            #
-# This file is part of RealTwin and is distributed under a __TBD__           #
+# This file is part of RealTwin and is distributed under a MIT               #
 # license. For the licensing terms see the LICENSE file in the top-level     #
 # directory.                                                                 #
 #                                                                            #
-# Contributors (Add you name below to acknowledge your contribution):        #
-# Xiangyong Roy Luo                                                          #
+# Contact: realtwin@ornl.gov                                                 #
 ##############################################################################
 
 """The real-twin developed by ORNL Applied Research and Mobility System (ARMS) group"""
