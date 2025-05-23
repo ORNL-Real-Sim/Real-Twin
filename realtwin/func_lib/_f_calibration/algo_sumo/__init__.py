808acba00a6028f36d5b8b338f1c97dbb460b9a0
xyluo25 <rochemay@163.com>
2025-05-23 11:50:16 -0400

    delete pygad implementation

--- realtwin/func_lib/_f_calibration/algo_sumo/__init__.py
+++
@@ -1,7 +0,0 @@
-'''
-##############################################################
-# Created Date: Wednesday, February 26th 2025
-# Contact Info: luoxiangyong01@gmail.com
-# Author/Copyright: Mr. Xiangyong Luo
-##############################################################
-'''
