119c7c90a905113bb08b29cf0a0a4e7a702ed542
xyluo25 <rochemay@163.com>
2025-05-29 17:26:00 -0400

    Add MIT license for the project

--- realtwin/__init__.py
+++ realtwin/__init__.py
@@ -3,7 +3,7 @@
 # Copyright (c) 2024, Oak Ridge National Laboratory                          #
 # All rights reserved.                                                       #
 #                                                                            #
-# This file is part of RealTwin and is distributed under a __TBD__           #
+# This file is part of RealTwin and is distributed under a MIT               #
 # license. For the licensing terms see the LICENSE file in the top-level     #
 # directory.                                                                 #
 #                                                                            #
